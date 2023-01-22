3c7f9cd1e9c19ef261f2efa8733109d23ba63743
Douglas <dglandre@gmail.com>
2023-01-22 14:05:10 -0300

    feat: Console com Injeção de Dependência

---
+++ ConsoleComInjecaoDependencia/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.DependencyInjection;
+using System.Security.Authentication.ExtendedProtection;
+
+Console.WriteLine("Hello, World!");
+
+// Criando uma coleção de serviços
+var serviceCollection = new ServiceCollection();
+
+// Configurando nossas classes na coleção de serviços
+serviceCollection.AddScoped<IOperacao, Operacao>();
+
+var serviceProvider = serviceCollection.BuildServiceProvider();
+
+var operacao = serviceProvider.GetRequiredService<IOperacao>();
+
+Console.WriteLine(operacao.Id);
+Console.ReadKey();
+
+interface IOperacao
+{
+    Guid Id { get; set; }
+}
+
+class Operacao : IOperacao
+{
+    public Operacao()
+    {
+        Id = Guid.NewGuid();
+    }
+    public Guid Id { get; set; }
+}
